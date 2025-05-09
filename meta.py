d6c827a9edf496220ffe8410093561004d8e0b6c
Shareen Arshad <107083486+shareenarshad-io@users.noreply.github.com>
2025-05-09 11:17:53 -0400

    new code

--- meta.py
+++ meta.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay
 
 
 '''
@@ -81,3 +82,244 @@ ax.bar_label(ax.containers[0])
 # Split the data into training and test data
 X_train, X_test, y_train, y_test = train_test_split(df_feature.drop(['tomatometer_status'], axis=1), df_feature.tomatometer_status, test_size= 0.2, random_state=42)
 print(f'Size of training data is {len(X_train)} and the size of test data is {len(X_test)}')
+
+#Decision Tree Classifier: Constrained to Three Leaf Nodes
+
+# Instantiate Decision Tree Classifier with max leaf nodes = 3
+tree_3_leaf = DecisionTreeClassifier(max_leaf_nodes= 3, random_state=2)
+
+# Train the classifier on the training data
+tree_3_leaf.fit(X_train, y_train)
+
+# Predict the test data with trained tree classifier
+y_predict = tree_3_leaf.predict(X_test)
+
+# Print accuracy and classification report on test data
+print(accuracy_score(y_test, y_predict))
+print(classification_report(y_test, y_predict))
+
+# Plot confusion matrix on test data
+#fig, ax = plt.subplots(figsize=(12, 9))
+#ConfusionMatrixDisplay.from_estimator(tree_3_leaf, X_test, y_test)
+
+# Visualize decision logic of decision tree model
+fig, ax = plt.subplots(figsize=(12, 9))
+plot_tree(tree_3_leaf, ax= ax)
+plt.show()
+
+#Decision Tree Classifier: Unconstrained
+
+# Instantiate Decision Tree Classifier with default hyperparameter settings
+tree = DecisionTreeClassifier(random_state=2)
+
+# Train the classifier on the training data
+tree.fit(X_train, y_train)
+
+# Predict the test data with trained tree classifier
+y_predict = tree.predict(X_test)
+
+# Print accuracy and classification report on test data
+print(accuracy_score(y_test, y_predict))
+print(classification_report(y_test, y_predict))
+
+
+#Random Forest Classifier
+# Instantiate Random Forest Classifier
+rf = RandomForestClassifier(random_state=2)
+
+# Train Random Forest Classifier on training data
+rf.fit(X_train, y_train)
+
+# Predict test data with trained model
+y_predict = rf.predict(X_test)
+
+# Print accuracy score and classification report
+print(accuracy_score(y_test, y_predict))
+print(classification_report(y_test, y_predict))
+
+# Get the feature importance
+feature_importance = rf.feature_importances_
+
+# Print feature importance
+for i, feature in enumerate(X_train.columns):
+    print(f'{feature} = {feature_importance[i]}')
+
+# Visualize feature from the most important to the least important
+indices = np.argsort(feature_importance)
+
+plt.figure(figsize=(12,9))
+plt.title('Feature Importances')
+plt.barh(range(len(indices)), feature_importance[indices], color='b', align='center')
+plt.yticks(range(len(indices)), [X_train.columns[i] for i in indices])
+plt.xlabel('Relative Importance')
+plt.show()
+
+# Random Forest Classifier with Feature Selection
+
+# Split data into train and test after feature selection
+X_train, X_test, y_train, y_test = train_test_split(df_feature.drop(['tomatometer_status', 'NR', 'runtime', 'PG-13', 'R', 'PG','G', 'NC17'], axis=1),df_feature.tomatometer_status, test_size= 0.2, random_state=42)
+print(f'Size of training data is {len(X_train)} and the size of test data is {len(X_test)}')
+
+# Initialize Random Forest class
+rf = RandomForestClassifier(random_state=2)
+
+# Train Random Forest on the training data after feature selection
+rf.fit(X_train, y_train)
+
+# Predict the traind model on the test data after feature selection
+y_predict = rf.predict(X_test)
+
+# Print the accuracy score and the classification report
+print(accuracy_score(y_test, y_predict))
+print(classification_report(y_test, y_predict))
+
+#Weighted Random Forest Classifier with Feature Selection
+
+# Check class distribution of target variable once more
+ax = df_feature.tomatometer_status.value_counts().plot(kind='bar', figsize=(12,9))
+ax.bar_label(ax.containers[0])
+
+# Compute class weight
+class_weight = compute_class_weight(class_weight= 'balanced', classes= np.unique(df_feature.tomatometer_status), 
+                      y = df_feature.tomatometer_status.values)
+
+class_weight_dict = dict(zip(range(len(class_weight.tolist())), class_weight.tolist()))
+class_weight_dict
+
+# Initialize Random Forest model with weight information
+rf_weighted = RandomForestClassifier(random_state=2, class_weight=class_weight_dict)
+
+# Train the model on the training data
+rf_weighted.fit(X_train, y_train)
+
+# Predict the test data with the trained model
+y_predict = rf_weighted.predict(X_test)
+
+#Print accuracy score and classification report
+print(accuracy_score(y_test, y_predict))
+print(classification_report(y_test, y_predict))
+
+
+# Second Approach: Predicting Movie Status Based on Review Sentiment
+# Read critics dataframe
+df_critics = pd.read_csv('rotten_tomatoes_critic_reviews_50k.csv')
+df_critics.head()
+
+# Merge critics dataframe with movie dataframe
+df_merged = df_critics.merge(df_movie, how='inner', on=['rotten_tomatoes_link'])
+df_merged = df_merged[['rotten_tomatoes_link', 'movie_title', 'review_content', 'review_type', 'tomatometer_status']]
+
+df_merged.head()
+
+# Drop entries with missing reviews
+df_merged = df_merged.dropna(subset=['review_content'])
+
+# Plot distribution of the review
+ax = df_merged.review_type.value_counts().plot(kind='bar', figsize=(12,9))
+ax.bar_label(ax.containers[0])
+
+# Pick only 5000 entries from the original dataset
+df_sub = df_merged[0:5000]
+
+# Encode the label
+review_type = pd.DataFrame(df_sub.review_type.replace(['Rotten','Fresh'],[0,1]))
+
+# Build final dataframe
+df_feature_critics = pd.concat([df_sub[['review_content']]
+                        ,review_type], axis=1).dropna()
+df_feature_critics.head()
+
+# Split data into training and test data
+X_train, X_test, y_train, y_test = train_test_split( df_feature_critics['review_content'], df_feature_critics['review_type'], test_size=0.2, random_state=42)
+
+#Default Random Forest
+
+# Instantiate vectorizer class
+vectorizer = CountVectorizer(min_df=1)
+
+# Transform our text data into vector
+X_train_vec = vectorizer.fit_transform(X_train).toarray()
+
+# Initialize random forest and train it
+rf = RandomForestClassifier(random_state=2)
+rf.fit(X_train_vec, y_train)
+
+# Predict and output classification report
+y_predicted = rf.predict(vectorizer.transform(X_test).toarray())
+
+print(classification_report(y_test, y_predicted))
+
+
+#Weighted Random Forest
+
+# Calculate class weight
+class_weight = compute_class_weight(class_weight= 'balanced', classes= np.unique(df_feature_critics.review_type), 
+                      y = df_feature_critics.review_type.values)
+
+class_weight_dict = dict(zip(range(len(class_weight.tolist())), class_weight.tolist()))
+class_weight_dict
+
+# Instantiate vectorizer class
+vectorizer = CountVectorizer(min_df=1)
+
+# Transform our text data into vector
+X_train_vec = vectorizer.fit_transform(X_train).toarray()
+
+# Initialize random forest and train it
+rf_weighted = RandomForestClassifier(random_state=2, class_weight=class_weight_dict)
+rf_weighted.fit(X_train_vec, y_train)
+
+# Predict and output classification report
+y_predicted = rf_weighted.predict(vectorizer.transform(X_test).toarray())
+
+print(classification_report(y_test, y_predicted))
+
+#Movie Status Prediction
+
+# Define a function to predict movie status based on the overall sentiment
+def predict_movie_status(prediction):
+    """Assign label (Fresh/Rotten) based on prediction"""
+    positive_percentage = (prediction == 1).sum()/len(prediction)*100
+    
+    prediction = 'Fresh' if positive_percentage >= 60 else 'Rotten'
+    
+    print(f'Positive review:{positive_percentage:.2f}%')
+    print(f'Movie status: {prediction}')
+
+#'Body of Lies' Prediction
+
+# Gather all of the reviews of Body of Lies movie
+df_bol = df_merged.loc[df_merged['movie_title'] == 'Body of Lies']
+df_bol.head()
+
+# Get the prediction
+y_predicted_bol = rf_weighted.predict(vectorizer.transform(df_bol['review_content']).toarray())
+predict_movie_status(y_predicted_bol)
+
+# Get the true label
+df_merged['tomatometer_status'].loc[df_merged['movie_title'] == 'Body of Lies'].unique()
+
+#'Angel Heart' Prediction
+# Gather all of the reviews of Angel Heart movie
+df_ah = df_merged.loc[df_merged['movie_title'] == 'Angel Heart']
+df_ah.head()
+
+# Get the prediction
+y_predicted_ah = rf_weighted.predict(vectorizer.transform(df_ah['review_content']).toarray())
+predict_movie_status(y_predicted_ah)
+
+# Get the true label
+df_merged['tomatometer_status'].loc[df_merged['movie_title'] == 'Angel Heart'].unique()
+
+#'The Duchess' Prediction
+
+# Gather all of the reviews of The Duchess movie 
+df_duchess = df_merged.loc[df_merged['movie_title'] == 'The Duchess']
+df_duchess.head()
+
+# Get the prediction
+y_predicted_duchess = rf_weighted.predict(vectorizer.transform(df_duchess['review_content']).toarray())
+predict_movie_status(y_predicted_duchess)
+
+# Get the true label
+df_merged['tomatometer_status'].loc[df_merged['movie_title'] == 'The Duchess'].unique()
